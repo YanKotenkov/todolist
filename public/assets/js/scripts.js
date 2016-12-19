56b30eecd90ff02b203090d318cd59eb60a73b95
YanKotenkov <013shadow@gmail.com>
2016-12-20 02:10:44 +0500

    Fixed bugs

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,42 +1,53 @@
+$('#task_input').focus();
+
+var taskCnt = Number(localStorage.getItem('taskCnt')) + 1;
+
+function renderTasks() {
+    $('#task_list').empty();
+    for (i = 0; i < taskCnt; i++) {
+        var taskId = "task-" + i;
+        if (localStorage.getItem(taskId) == null) {
+            continue;
+        } else {
+            addTask(taskId, i);
+        }
+        console.log("taskId = ", taskId, "storage item = ", localStorage.getItem(taskId));
+    }
+}
+
 function addTask(taskId, i) {
-    $('#taskList').append("<li data-id=" + i + " class='well well-small'>" + localStorage.getItem(taskId) +
+    $('#task_list').append("<li data-id=" + i + " class='well well-small'>" + localStorage.getItem(taskId) +
         " <button data-id=" + i + " class='taskRemove btn btn-danger'>Удалить</button>" +
         "</li>");
 }
 
-$(function() {
-    var i = 0;
-    for (i = 0; i < localStorage.length; i++) {
-        var taskId = "task-" + i;
-        addTask(taskId, i);
-        console.log("taskId = ", taskId, "storage item = ", localStorage.getItem(taskId));
-    }
-
-    $('#clear').click(function () {
-        localStorage.clear();
-        $('div#taskList').empty();
-    });
-
-    $('#taskInput').keyup(function(event){
-        if((event.keyCode == 13) && ($('#taskInput').val() != "")){
-            var taskId = "task-" + i;
-            var taskMessage = $('#taskInput').val();
-            localStorage.setItem(taskId, taskMessage);
-            addTask(taskId, i);
-            var task = $('#' + taskId);
-            task.css('display', 'none');
-            task.slideDown('fast');
-            $('#taskInput').val('');
-            i++;
-        }
-        return false;
-    });
-
-    $(document).on('click', '.taskRemove', function (event) {
-        taskRemove = 'task-' + $(this).data("id");
-        localStorage.removeItem(taskRemove);
-        $(this).slideUp('fast', function () {
-            $(this).parent().remove();
-        })
-    });
+$('#clear').click(function () {
+    localStorage.clear();
+    $('#task_list').empty();
+    taskCnt = 0;
 });
+
+$('#task_input').keyup(function(event){
+    if((event.keyCode == 13) && ($('#task_input').val() != "")){
+        var taskId = "task-" + taskCnt;
+        var taskMessage = $('#task_input').val();
+        localStorage.setItem(taskId, taskMessage);
+        localStorage.setItem('taskCnt', taskCnt);
+        addTask(taskId, i);
+        var task = $('#' + taskId);
+        task.css('display', 'none').slideDown('fast');
+        $('#task_input').val('').focus();
+        taskCnt++;
+    }
+    return false;
+});
+
+$(document).on('click', '.taskRemove', function (event) {
+    taskRemove = 'task-' + $(this).data("id");
+    localStorage.removeItem(taskRemove);
+    $(this).slideUp('fast', function () {
+        $(this).parent().remove();
+    })
+});
+
+renderTasks();
