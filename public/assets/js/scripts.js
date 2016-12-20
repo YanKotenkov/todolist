a83bc3ce605fe1163f53b83b2881853d8c975e5f
YanKotenkov <013shadow@gmail.com>
2016-12-20 20:57:49 +0500

    fixed bugs

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -2,26 +2,36 @@ $('#task_input').focus();
 
 var taskCnt = Number(localStorage.getItem('taskCnt')) + 1;
 var list = '';
+var id = 0;
+var data = {
+    id: 0,
+    taskId: 0,
+    taskMessage: 0,
+    isDone: ''
+};
 
 function renderTasks() {
+    $('#task_list').empty();
     for (i = 0; i < taskCnt; i++) {
-        var taskId = "task-" + i;
-        if (localStorage.getItem(taskId) == null) {
-            continue;
-        } else {
-            addTask(taskId, i, '#task_list');
-        }
-        console.log("taskId = ", taskId, "storage item = ", localStorage.getItem(taskId));
+        taskId = "task-" + i;
+        data = JSON.parse(localStorage.getItem(taskId));
+        if (localStorage.getItem(taskId) != null) {
+            var id = data.id;
+            var taskMessage = data.taskMessage;
+            var done = data.isDone;
+            addTask(taskMessage, id, done);
+        } else continue;
     }
+    $('#task_input').focus();
 }
 
-function addTask(taskId, i) {
-    $('#task_list').append("<li data-id=" + i + " class='well well-small'>" + localStorage.getItem(taskId) +
-        "<a href='#' data-id=" + i + "  class='task-remove'>" +
-        "<span class='glyphicon glyphicon-remove' style='color:red;'></span>" +
+function addTask(taskMessage, id, done) {
+    $('#task_list').append("<li data-id=" + id + " class = 'list-group-item " + done + "'>" + taskMessage +
+        "<a href='#' data-id=" + id + "  class='task-remove'>" +
+        "<span class='glyphicon glyphicon-remove'></span>" +
         "</a>" +
-        "<a href='#' data-id=" + i + "  class='task-complete' style='color: grey'>" +
-        "<span class='glyphicon glyphicon-ok'></span>" +
+        "<a href='#' data-id=" + id + "  class='task-complete'>" +
+        "<span class='glyphicon glyphicon-ok " + done + "'></span>" +
         "</a>" +
         "</li>");
 }
@@ -36,29 +46,48 @@ $('#task_input').keyup(function(event){
     if((event.keyCode == 13) && ($('#task_input').val() != "")){
         var taskId = "task-" + taskCnt;
         var taskMessage = $('#task_input').val();
-        localStorage.setItem(taskId, taskMessage);
+        data = {
+            id : taskCnt,
+            taskId : taskId,
+            taskMessage : taskMessage,
+            isDone : ''
+        };
+        localStorage.setItem(taskId, JSON.stringify(data));
         localStorage.setItem('taskCnt', taskCnt);
-        addTask(taskId, i);
-        var task = $('#' + taskId);
-        task.css('display', 'none').slideDown('normal');
-        $('#task_input').val('').focus();
         taskCnt++;
+        renderTasks();
+        $('#task_input').val('');
     }
     return false;
 });
 
 $(document.body).on('click', '.task-remove', function (event) {
-    taskRemove = 'task-' + $(this).data("id");
+    taskRemove = 'task-' + $(this).attr("data-id");
     localStorage.removeItem(taskRemove);
-    $(this).slideUp('normal', function () {
         $(this).parent().remove();
-    });
-    $('#task_input').focus();
+    renderTasks();
 });
 
 $(document.body).on('click', '.task-complete', function (event) {
-    $(this).attr('style', 'color:green');
-    $(this).parent().attr('style', 'text-decoration:line-through');
+    taskId = 'task-' + $(this).attr("data-id");
+    var dataTmp = {
+        id: 0,
+        taskId: 0,
+        taskMessage: 0,
+        isDone: ''
+    };
+    for (i = 1; i < taskCnt; i++) {
+        taskId = 'task-' + i;
+        dataTmp = JSON.parse(localStorage.getItem(taskId));
+        if (localStorage.getItem(taskId) != null) {
+            if (dataTmp.id == $(this).attr("data-id")) {
+                break;
+            }
+        } else continue;
+    }
+    dataTmp.isDone = 'done';
+    localStorage.setItem(taskId, JSON.stringify(dataTmp));
+    renderTasks();
 });
 
 renderTasks();
