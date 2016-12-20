3f73a7f5c8287f35a85999ed60db1c3b6d077e5f
YanKotenkov <013shadow@gmail.com>
2016-12-20 18:00:27 +0500

    Added completed button

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,15 +1,15 @@
 $('#task_input').focus();
 
 var taskCnt = Number(localStorage.getItem('taskCnt')) + 1;
+var list = '';
 
 function renderTasks() {
-    $('#task_list').empty();
     for (i = 0; i < taskCnt; i++) {
         var taskId = "task-" + i;
         if (localStorage.getItem(taskId) == null) {
             continue;
         } else {
-            addTask(taskId, i);
+            addTask(taskId, i, '#task_list');
         }
         console.log("taskId = ", taskId, "storage item = ", localStorage.getItem(taskId));
     }
@@ -17,14 +17,19 @@ function renderTasks() {
 
 function addTask(taskId, i) {
     $('#task_list').append("<li data-id=" + i + " class='well well-small'>" + localStorage.getItem(taskId) +
-        " <button data-id=" + i + " class='taskRemove btn btn-danger'>Удалить</button>" +
+        "<a href='#' data-id=" + i + "  class='task-remove'>" +
+        "<span class='glyphicon glyphicon-remove' style='color:red;'></span>" +
+        "</a>" +
+        "<a href='#' data-id=" + i + "  class='task-complete' style='color: grey'>" +
+        "<span class='glyphicon glyphicon-ok'></span>" +
+        "</a>" +
         "</li>");
 }
 
 $('#clear').click(function () {
     localStorage.clear();
     $('#task_list').empty();
-    taskCnt = 0;
+    $('#task_input').focus();
 });
 
 $('#task_input').keyup(function(event){
@@ -35,19 +40,25 @@ $('#task_input').keyup(function(event){
         localStorage.setItem('taskCnt', taskCnt);
         addTask(taskId, i);
         var task = $('#' + taskId);
-        task.css('display', 'none').slideDown('fast');
+        task.css('display', 'none').slideDown('normal');
         $('#task_input').val('').focus();
         taskCnt++;
     }
     return false;
 });
 
-$(document).on('click', '.taskRemove', function (event) {
+$(document.body).on('click', '.task-remove', function (event) {
     taskRemove = 'task-' + $(this).data("id");
     localStorage.removeItem(taskRemove);
-    $(this).slideUp('fast', function () {
+    $(this).slideUp('normal', function () {
         $(this).parent().remove();
-    })
+    });
+    $('#task_input').focus();
+});
+
+$(document.body).on('click', '.task-complete', function (event) {
+    $(this).attr('style', 'color:green');
+    $(this).parent().attr('style', 'text-decoration:line-through');
 });
 
 renderTasks();
